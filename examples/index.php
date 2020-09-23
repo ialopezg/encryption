5f4c5f6e7745c52ca006f2f9b737b2add757cfa1
Isidro A. López G <me@ialopezg.com>
2020-09-23 17:42:27 -0500

    Added examples directory.

---
+++ examples/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$password = 'The1@ngel';
+
+// Display the original string
+echo "Original password: <strong>{$password}</strong><br>";
+
+$key = \ialopezg\Libraries\Encryption\Password::createKey(8);
+$encrypter = new \ialopezg\Libraries\Encryption\Password([
+    'cipher' => 'AES-128-CTR',
+    'digest' => 'SHA512',
+    'options' => OPENSSL_CIPHER_RC2_40,
+    'key' => $key
+]);
+$encrypted_password = $encrypter->encrypt($password);
+// Display the encrypted string
+echo "Encrypted password: <strong>{$encrypted_password}</strong><br>";
+
+// Display the encrypted string
+echo 'Password are equals: <strong>' . ($encrypter->verify($password, $encrypted_password) ? 'true' : 'false') . '</strong>';
